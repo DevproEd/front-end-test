b352541262a6faa9cbc189cb57a48c9f694e404a
Ed Lin <ed.lin@devpro.com.tw>
2024-10-02 10:49:17 +0800

    add pre-commit setting and vscode setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'front-end-test';
+    title = 'front-end-test';
+    ngOnInit(): void {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        this.title = 'front-end-pre-commit-test';
+    }
 }
---
+++ src/app/shared/app.model.ts
@@ -0,0 +1,5 @@
+export interface testModel {
+    aa: string;
+    bb: string;
+    cc: boolean;
+}
